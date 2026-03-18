c37e6cafa9917251e53dd202093729afb08c6faf
bainemmanuel86-hub <bainemmanuel86@gmail.com>
2026-03-18 23:02:39 +0530

    revert app.py to original

--- app.py
+++ app.py
@@ -7,11 +7,4 @@ Use this file as an alternate entrypoint:
 It delegates to the maintained app implementation in `app_main_pharma.py`.
 """
 
-import streamlit as st
-
-if "startup_done" not in st.session_state:
-    import startup
-    startup.main()
-    st.session_state.startup_done = True
-
 from app_main_pharma import *  # noqa: F401,F403
